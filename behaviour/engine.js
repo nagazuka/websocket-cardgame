8485e3b5d4879341419f35efe5c619e3e485c3d8
Shanny Anoep <s.anoep@gmail.com>
2011-09-23 15:41:07 +0200

    Replaced hardcoded x-y with calculations

--- behaviour/engine.js
+++ behaviour/engine.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-    var paper = Raphael("canvas", 550, 450);
+    var WIDTH = 650
+    var HEIGHT = 450
+    var paper = Raphael("canvas", WIDTH, HEIGHT);
+    
     // rectangle with rounded corrners
-    //
-    var bg = paper.rect(0, 0, 550, 450);
+    var bg = paper.rect(0, 0, WIDTH, HEIGHT);
     bg.attr({fill: "45-#000-#555"});
-    var table = paper.image("images/green_poker_skin.png", 20, 30, 500, 350);
+
+    var TABLE_WIDTH = 500;
+    var TABLE_HEIGHT = 350;
+    var TABLE_X = (WIDTH - TABLE_WIDTH) / 2
+    var TABLE_Y = (HEIGHT - TABLE_HEIGHT) / 2
+    var table = paper.image("images/green_poker_skin.png", TABLE_X, TABLE_Y, TABLE_WIDTH, TABLE_HEIGHT);
 
     for (i=2; i < 10; i++) {
-      var card = paper.image("images/cards/simple_c_" + i + ".svg.png", 100 + i*20, 320, 45, 70);
+      var card = paper.image("images/cards/simple_c_" + i + ".svg.png", TABLE_X + i*20, TABLE_Y + TABLE_HEIGHT - 50, 45, 70);
     }
 
     var ws = new WebSocket("ws://localhost:8888/websocket");
