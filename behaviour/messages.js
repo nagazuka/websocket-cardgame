61c48b07a25d33ae22480c8419da9a7b1a847d8e
Shanny Anoep <s.anoep@gmail.com>
2012-02-07 10:29:47 +0100

    Fixed missing comma

--- behaviour/messages.js
+++ behaviour/messages.js
@@ -5,7 +5,7 @@ var messages = {
     yourTurn:  'Jij bent aan de beurt...',
     youWinHand:  'Jij hebt deze hand gemaakt!',
     otherWinsHand: '\nheeft deze hand gemaakt!',
-    errorMessage:  'Ai ai ai!\nEr is een fout opgetreden.'
+    errorMessage:  'Ai ai ai!\nEr is een fout opgetreden.',
     invalidMove:  'Ongeldige kaart.\nKies een andere...'
   }
 };
